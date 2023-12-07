2f0b5cd15a4d7b09f1169d43ae485f343e82837a
Jude Muriithi <90065670+jkmuriithi@users.noreply.github.com>
2023-12-07 11:14:57 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 // Vite config settings go here
 export default defineConfig({
     // Base URL MUST match the name of your repository for GitHub Pages to work
-    base: '/cos426repotest/',
+    base: '/roguelife426/',
 });
