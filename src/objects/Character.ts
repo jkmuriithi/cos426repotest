3ceff3a5a6a8671aececc56936397001cf23d41d
Jude Muriithi <jkmuriithi11@gmail.com>
2023-12-08 16:42:10 -0500

    Implement basic jump controls

--- src/objects/Character.ts
+++ src/objects/Character.ts
@@ -1,4 +1,4 @@
-import { Body, Box as CannonBox, Vec3 } from 'cannon-es';
+import { BODY_TYPES, Body, Box as CannonBox, Vec3 } from 'cannon-es';
 import {
     BoxGeometry,
     Group,
@@ -11,6 +11,7 @@ import {
 class Character extends Group {
     body: Body;
     size: [number, number, number] = [1, 3, 1];
+    canJump: boolean = false;
 
     constructor() {
         // Call parent Group() constructor
@@ -27,9 +28,27 @@ class Character extends Group {
 
         // Add physics
         this.body = new Body({
-            mass: 10,
+            mass: 1,
             position: new Vec3(...position),
             shape: new CannonBox(new Vec3(...this.size.map((n) => n / 2))),
+        });
+
+        // Jumping controls
+        this.body.addEventListener('collide', (e: { body: Body }) => {
+            // console.log(e);
+            if (
+                e.body.type === BODY_TYPES.STATIC &&
+                e.body.position.y < this.body.position.y
+            ) {
+                this.canJump = true;
+            }
+        });
+
+        window.addEventListener('keydown', (e) => {
+            if (this.canJump && e.key === ' ') {
+                this.body.velocity.y += 10;
+                this.canJump = false;
+            }
         });
     }
 
