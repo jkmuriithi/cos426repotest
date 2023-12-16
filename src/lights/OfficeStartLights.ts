5349ed6e0cde7ef1bc4330e261ccc78f4e8472b5
Jude Muriithi <jkmuriithi11@gmail.com>
2023-12-15 21:48:00 -0500

    Relight office

--- src/lights/OfficeStartLights.ts
+++ src/lights/OfficeStartLights.ts
@@ -1,4 +1,4 @@
-import { Group, HemisphereLight } from 'three';
+import { Group, HemisphereLight, SpotLight } from 'three';
 
 import { COLORS, SHADOW_MAP_SIZE } from '../globals';
 
@@ -8,11 +8,33 @@ class OfficeStartLights extends Group {
     constructor() {
         // Invoke parent Group() constructor
         super();
+        const spotPositions = [
+            [10, 11.25],
+            [-10, 11.25],
+            [10, -11.25],
+            [-10, -11.25],
+        ];
+
+        for (const pos of spotPositions) {
+            const spot = new SpotLight(
+                COLORS.WHITE,
+                200,
+                0,
+                Math.PI / 3,
+                0.9,
+                2
+            );
+            spot.position.set(pos[0], 20, pos[1]);
+            spot.target.position.set(pos[0], 0, pos[1]);
+            spot.castShadow = true;
+            spot.shadow.mapSize.width = this.shadowMapSize;
+            spot.shadow.mapSize.height = this.shadowMapSize;
+            this.add(spot, spot.target);
+        }
 
         const hemi = new HemisphereLight(COLORS.WHITE, COLORS.WHITE, 1);
         hemi.position.set(-10, 10, 0);
         this.add(hemi);
-        
     }
 }
 
